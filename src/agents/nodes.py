bbe43a4d26bd4763a31b70212f41ed33a31d4f7f
Anish Acharya <anishacharya@utexas.edu>
2021-01-10 20:38:57 -0600

    +

--- src/agents/nodes.py
+++ src/agents/nodes.py
@@ -28,35 +28,22 @@ class Agent:
 class FedClient(Agent):
     def __init__(self,
                  client_id: int,
-                 client_config: Dict,
-                 learner,
-                 mal: bool = False):
-        """
-        :client_id: int , Specify the Client ID
-        :config:  , pass parsed json (Dict) as config ; can have it personalized per client
-        :learner: , pass the initialized model; the client does k local steps starting from this model. Usually same
-         for each client in the Federated/Decentralized Setting
-        :mal:bool, If True, Client behaves as adversary as described in the adversary rule in config
-        """
+                 learner):
+        """ Implements a Federated Client Node """
         Agent.__init__(self)
         self.client_id = client_id
-        self.config = client_config
         self.learner = learner
 
-        self.error_feedback = client_config.get('error_feedback', False)
-        self.mal = mal
+        self.w_current = None
+        self.w_old = None
 
-        self.opt = get_optimizer(params=self.learner.parameters(), optimizer_config=self.config)
-        self.lrs = get_scheduler(optimizer=self.opt)
-        self.criterion = get_loss(loss=self.config.get('criterion', 'cross_entropy'))
+        self.grad_current = None
+        self.grad_stale = None
 
-        self.current_w = flatten_params(learner=self.learner)
-        self.current_e = np.zeros_like(self.current_w)
-        self.grad = None
         self.x_train = None
         self.y_train = None
 
-    def train_step(self, x_train=None, y_train=None, num_iter=1, device="cpu"):
+    def train_step(self, num_iter=1, device="cpu"):
         pass
 
 
@@ -67,9 +54,8 @@ class FedServer(Agent):
         self.learner = server_model
         self.gar = gar
 
-        # initialize current w
-        params = flatten_params(learner=self.learner)
-        self.w_current = params
+        # initialize params
+        self.w_current = None
         self.w_old = None
         self.u = None
 
