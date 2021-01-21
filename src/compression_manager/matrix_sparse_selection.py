ae55aaf282dc141ef9d2b5b61e07c101c313aafb
Anish Acharya <anishacharya@utexas.edu>
2021-01-21 01:49:19 -0600

    +

--- src/compression_manager/matrix_sparse_selection.py
+++ src/compression_manager/matrix_sparse_selection.py
@@ -42,10 +42,10 @@ class SparseApproxMatrix:
         # Invoke Sampling algorithm here
         # --------------------------------- #
         if self.sampling_rule == 'active_norm':
-            print('Applying active norm column sampling on gradients')
+            # print('Applying active norm column sampling on gradients')
             I_k = self._active_norm_sampling(G=G)
         elif self.sampling_rule == 'random':
-            print('Applying random column sampling on gradients')
+            # print('Applying random column sampling on gradients')
             I_k = self._random_sampling(d=d if self.axis == 0 else n)
         else:
             return G
