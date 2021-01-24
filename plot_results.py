9bae297d88c4665bbd93bfcea0841751af53ae38
Anish Acharya <anishacharya@utexas.edu>
2021-01-23 22:26:54 -0600

    +

--- plot_results.py
+++ plot_results.py
@@ -24,6 +24,21 @@ def plot_driver(label: str, res_file: str, plt_type: str = 'epoch_loss',
     plt.plot(x, res, label=label, linewidth=line_width, marker=marker, linestyle=line_style)
 
 
+def plot_timing(res_file: str, label, line_width=2, marker=None, line_style=None):
+    # d = [100, 1000, 10000, 100000]
+    with open(res_file, 'rb') as f:
+        res = json.load(f)
+    d = list(res.keys())
+    t = list(res.values())
+    ax = plt.gca()
+
+    plt.yscale('log')
+    #ax.set_xscale('log')
+    plt.scatter(d, t, label=label)  #, linewidth=line_width, marker=marker, linestyle=line_style)
+    plt.plot(d, t, linestyle='dashed')
+
+
+
 def plot_metrics():
     # -------------------------------
     # ** Usually No Need to Modify **
@@ -33,40 +48,30 @@ def plot_metrics():
 
     # -------------------------------------------------------------------------------------------
     # ------------------------------- Modify Here -----------------------------------------------
-    d = 'result_dumps/Robust/distributed/fashion_mnist/'
+    d = 'result_dumps/timing_exp/'
     o = [
         'mean',
-        # 'geo_med',
-        'mean.norm_0.1',
-        'mean.norm_0.1.ef',
-        # 'geo_med.norm_0.1',
-        # 'geo_med.norm_0.1.ef',
-        'mean.norm_0.05',
-        'mean.norm_0.05.ef',
-        # 'geo_med.norm_0.05',
-        # 'geo_med.norm_0.05.ef'
+        'geo_med',
+        'geo_med.norm_0.1'
 
     ]
-    labels = ['Mini-Batch SGD',
-              # 'GM SGD',
-              'Sparse SGD (k=10%)',
-              'Sparse SGD + Memory (k=10%)',
-              # 'Co-Sparse GM SGD (k=10%)',
-              # 'Co-Sparse GM EC-SGD (k=10%)',
-              'Sparse SGD (k=5%)',
-              'Sparse SGD + Memory (k=5%)',
-              # 'Co-Sparse GM SGD (k=5%)',
-              # 'Co-Sparse GM EC-SGD (k=5%)'
+    labels = [
+        'Mean',
+        'GM',
+        '10-sGM'
               ]
 
-    plot_type = 'train_loss'
+    plot_type = 'timing'
     sampling_freq = 5
 
     for op, label in zip(o, labels):
         result_file = d + op
-        plot_driver(label=label, res_file=result_file,
-                    plt_type=plot_type, optima=0, line_width=4,
-                    sampling_freq=sampling_freq)
+        if plot_type is 'timing':
+            plot_timing(label=label, res_file=result_file, line_width=4)
+        else:
+            plot_driver(label=label, res_file=result_file,
+                        plt_type=plot_type, optima=0, line_width=4,
+                        sampling_freq=sampling_freq)
     # -------------------------------------------------------------------------------------------
     # -------------------------------
     # ** Usually No Need to Modify **
@@ -98,6 +103,10 @@ def plot_metrics():
         plt.ylabel('Train Error', fontsize=10)
         plt.xlabel('Communication Rounds', fontsize=10)
 
+    elif plot_type is 'timing':
+        plt.ylabel('Time', fontsize=10)
+        plt.xlabel('Dimension', fontsize=10)
+
     else:
         raise NotImplementedError
 
@@ -116,16 +125,21 @@ def get_runtime(gar, X, repeat: int = 10):
     return T
 
 
-def compare_gar_speed(agg_config: Dict, column_sampling=None):
-    d = [100, 1000, 10000, 100000]
+def compare_gar_speed(agg_config: Dict,
+                      sparse_approximation_config=None):
+    # d = [100, 1000, 10000, 100000]
+    d = [100000, 1000000, 10000000, 100000000]
     n = 500
     res = {}
     gar = get_gar(aggregation_config=agg_config)
+
     for dim in d:
+        sparse_approx_op = SparseApproxMatrix(conf=sparse_approximation_config)
         # generate n points in d dimensions
         X = np.random.normal(0, 0.3, (n, dim))
-        if column_sampling is not None:
-            X_sparse = column_sampling.sparse_approx(G=X)
+        if sparse_approximation_config["rule"] is not None:
+            print('Running with sparse G approx')
+            X_sparse = sparse_approx_op.sparse_approx(G=X)
             t = get_runtime(gar=gar, X=X_sparse, repeat=3)
         else:
             t = get_runtime(gar=gar, X=X, repeat=3)
@@ -138,7 +152,7 @@ def runtime_exp():
     op_file = 'result_dumps/timing_exp/geo_med.norm_0.1'
     aggregation_config = \
         {
-            "gar": "geo_med",
+            "gar": "mean",
             "trimmed_mean_config": {"proportion": 0.1},
             "glomo_config": {"glomo_server_c": 1},
         }
@@ -149,18 +163,13 @@ def runtime_exp():
         "frac_coordinates": 0.1,
     }
 
-    sparse_approx = SparseApproxMatrix(conf=sparse_approximation_config)
-
-    if sparse_approximation_config["rule"] is not None:
-        print('Running with sparse G approx')
-        results = compare_gar_speed(agg_config=aggregation_config, column_sampling=sparse_approx)
-    else:
-        results = compare_gar_speed(agg_config=aggregation_config)
+    results = compare_gar_speed(agg_config=aggregation_config,
+                                sparse_approximation_config=sparse_approximation_config)
 
     with open(op_file, 'w+') as f:
         json.dump(results, f, indent=4, ensure_ascii=False, cls=NumpyEncoder)
 
 
 if __name__ == '__main__':
-    # plot_metrics()
-    runtime_exp()
+    plot_metrics()
+    # runtime_exp()
