3f0c4eccffca4cf39ddcf3a33a9370c25a59b13b
Anish Acharya <anishacharya@utexas.edu>
2021-02-01 16:58:27 -0600

    +

--- src/model_manager/resnet.py
+++ src/model_manager/resnet.py
@@ -73,14 +73,26 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False)
+        self.conv1 = nn.DataParallel(self.conv1)
+
         self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = nn.DataParallel(self.bn1)
+
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer1 = nn.DataParallel(self.layer1)
+
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer2 = nn.DataParallel(self.layer2)
+
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer3 = nn.DataParallel(self.layer3)
+
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer4 = nn.DataParallel(self.layer4)
+
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.DataParallel(self.linear)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
