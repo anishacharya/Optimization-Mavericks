c026116c10f0b0655a9732404d59afa606dc90e1
Anish Acharya <anishacharya@utexas.edu>
2021-02-03 06:35:41 -0600

    +

--- timing_exp.py
+++ timing_exp.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # Hyper Params
     directory = 'result_dumps/timing_exp/'
     algo = 'BGMD'
-    op_file = 'geo_med'
+    op_file = 'ours.0.05'
 
     d = [int(1e3), int(5e3), int(1e4), int(5e4)]
     n = 5000
