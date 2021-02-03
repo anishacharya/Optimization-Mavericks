62703f0573d5eee24421c63397f837f3324a8d26
Anish Acharya <anishacharya@utexas.edu>
2021-02-03 06:25:40 -0600

    +

--- timing_exp.py
+++ timing_exp.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     res = {}
     agg_config = \
         {
-            "gar": "gm",
+            "gar": "geo_med",
             "krum_config": {"krum_frac": 0.3},
         }
     gar = get_gar(aggregation_config=agg_config)
