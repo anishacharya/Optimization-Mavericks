71625e3e4ed523bbe5a8b44f552dfb9e92c5aab8
Anish Acharya <anishacharya@utexas.edu>
2021-02-04 10:54:37 -0600

    +

--- plot_results.py
+++ plot_results.py
@@ -92,11 +92,12 @@ def plot_metrics():
 
     # -------------------------------------------------------------------------------------------
     # ------------------------------- Modify Here -----------------------------------------------
-    d = 'result_dumps/distributed/fashion_mnist/rerun/'
+    # d = 'result_dumps/distributed/fashion_mnist/rerun/'
+    d = 'result_dumps/fed/'
     o = [
-        'mean',
-        'gm',
-        'ours'
+        'mean.0.1',
+        'gm.0.1',
+        'ours.0.1'
     ]
     labels = [
         r"\textsc{SGD}",
@@ -148,7 +149,7 @@ def plot_metrics():
         plt.xlabel('Communication Rounds', fontsize=10)
         plt.yscale('log')
         # plt.xlim(left=0, right=375 * 5)
-        plt.ylim(top=1)
+        plt.ylim(top=5)
 
     elif plot_type is 'train_error':
         plt.ylabel('Train Error', fontsize=10)
