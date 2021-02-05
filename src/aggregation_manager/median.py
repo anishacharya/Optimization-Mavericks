601c716368f085c1d6e3f0d9c0d0df831db9d50e
Anish Acharya <anishacharya@utexas.edu>
2021-02-05 04:26:18 -0600

    +

--- src/aggregation_manager/median.py
+++ src/aggregation_manager/median.py
@@ -48,6 +48,7 @@ def vardi(X, eps=1e-5) -> np.ndarray:
         if num_zeros == 0:
             mu1 = T
         elif num_zeros == len(X):
+            print('Time Taken For GM'.format(time.time() - t0))
             return mu
         else:
             r = np.linalg.norm((T - mu) * sum(D_inv))
@@ -58,8 +59,6 @@ def vardi(X, eps=1e-5) -> np.ndarray:
             print('Time Taken For GM'.format(time.time() - t0))
             return mu1
         mu = mu1
-
-
 
 
 if __name__ == '__main__':
