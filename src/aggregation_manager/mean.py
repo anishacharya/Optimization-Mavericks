88007da3d0b8dc8a497e2ee462f3f6e55fbe81fb
Anish Acharya <anishacharya@utexas.edu>
2021-02-05 21:16:20 -0600

    +

--- src/aggregation_manager/mean.py
+++ src/aggregation_manager/mean.py
@@ -6,6 +6,7 @@ g = mean(g_i) Regular Mini-Batch SGD
 """
 from .base import GAR
 import numpy as np
+from typing import List
 
 
 class Mean(GAR):
@@ -13,6 +14,11 @@ class Mean(GAR):
     def __init__(self, aggregation_config):
         GAR.__init__(self, aggregation_config=aggregation_config)
 
-    def aggregate(self, G: np.ndarray) -> np.ndarray:
-        agg_grad = self.weighted_average(stacked_grad=G)
-        return agg_grad
+    def aggregate(self, G: np.ndarray, ix: List[int] = None) -> np.ndarray:
+        # if ix given only aggregate along the indexes ignoring the rest of the ix
+        g_agg = np.zeros_like(G[0, :])
+        if ix is not None:
+            G = G[:, ix]
+
+        g_agg[:, ix] = self.weighted_average(stacked_grad=G)
+        return g_agg
