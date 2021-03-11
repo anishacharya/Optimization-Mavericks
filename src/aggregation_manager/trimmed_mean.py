9b1f33439bfa156cf591136ac3954fd258671376
Anish Acharya <anishacharya@utexas.edu>
2021-03-11 01:10:35 -0600

    +

--- src/aggregation_manager/trimmed_mean.py
+++ src/aggregation_manager/trimmed_mean.py
@@ -3,6 +3,7 @@
 import numpy as np
 from .base import GAR
 from scipy import stats
+from typing import List
 """
 Computes Trimmed mean estimates
 Cite: Yin, Chen, Ramchandran, Bartlett : Byzantine-Robust Distributed Learning: Towards Optimal Statistical Rates 
@@ -14,7 +15,11 @@ class TrimmedMean(GAR):
         GAR.__init__(self, aggregation_config=aggregation_config)
         self.trimmed_mean_config = aggregation_config.get('trimmed_mean_config', {})
         self.proportion = self.trimmed_mean_config.get('proportion', 0.1)
+        self.axis = self.trimmed_mean_config.get('axis', 0)
 
-    def aggregate(self, G: np.ndarray) -> np.ndarray:
-        agg_grad = stats.trim_mean(a=G, proportiontocut=self.proportion)
-        return agg_grad
+    def aggregate(self, G: np.ndarray, ix: List[int] = None) -> np.ndarray:
+        agg_grad = stats.trim_mean(a=G, proportiontocut=self.proportion, axis=self.axis)
+        if ix is not None:
+            return agg_grad[ix]
+        else:
+            return agg_grad
