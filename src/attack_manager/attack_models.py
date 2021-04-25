49d348f6a29f22a8213c6a5ee67005abc5fe73a1
Anish Acharya <anishacharya@utexas.edu>
2021-04-25 02:02:56 -0500

    +

--- src/attack_manager/attack_models.py
+++ src/attack_manager/attack_models.py
@@ -171,7 +171,7 @@ class BitFlipAttack(ByzAttack):
         ByzAttack.__init__(self, attack_config=attack_config)
 
     def attack(self, g):
-        return - 10 * g
+        return - 5 * g
 
 
 class RandomSignFlipAttack(ByzAttack):
