47a567480da3b68985051b47d28c23f96bc9335d
Anish Acharya <anishacharya@utexas.edu>
2021-04-24 22:37:56 -0500

    +

--- src/aggregation_manager/median.py
+++ src/aggregation_manager/median.py
@@ -26,18 +26,26 @@ class GeometricMedian(GAR):
     def __init__(self, aggregation_config):
         GAR.__init__(self, aggregation_config=aggregation_config)
         self.geo_med_config = aggregation_config.get('geo_med_config', {})
-        self.geo_med_alg = self.geo_med_config.get('geo_med_alg', 'vardi')
+        self.geo_med_alg = self.geo_med_config.get('geo_med_alg', 'weiszfeld')
+
+    def get_gm(self, X: np.ndarray):
+        if self.geo_med_alg == 'vardi':
+            return vardi(X=X)
+        elif self.geo_med_alg == 'weiszfeld':
+            return weiszfeld(X=X)
+        else:
+            raise NotImplementedError
 
     def aggregate(self, G: np.ndarray, ix: List[int] = None) -> np.ndarray:
         # if ix given only aggregate along the indexes ignoring the rest of the ix
         if ix is not None:
             g_agg = np.zeros_like(G[0, :])
             G = G[:, ix]
-            low_rank_gm = vardi(X=G)
+            low_rank_gm = self.get_gm(X=G)
             g_agg[ix] = low_rank_gm
             return g_agg
         else:
-            return vardi(X=G)
+            return self.get_gm(X=G)
 
 
 def weiszfeld(X, eps=1e-5, max_iter=1000):
@@ -51,8 +59,18 @@ def weiszfeld(X, eps=1e-5, max_iter=1000):
     num_iter = 0
     while num_iter < max_iter:
         # noinspection PyTypeChecker
-        D = cdist(X, [mu]).astype(mu.dtype)
-    pass
+        distances = cdist(X, [mu]).astype(mu.dtype)
+        distances = np.where(distances == 0, 1, distances)
+        mu1 = (X / distances).sum(axis=0) / (1. / distances).sum(axis=0)
+        guess_movement = np.sqrt(((mu - mu1) ** 2).sum())
+
+        mu = mu1
+
+        if guess_movement <= eps:
+            break
+        num_iter += 1
+
+    return mu
 
 
 def vardi(X, eps=1e-5, max_iter=15) -> np.ndarray:
