e89b50339f11077c794f7fc406952e983b17b631
Anish Acharya <anishacharya@utexas.edu>
2021-05-27 19:47:01 -0700

    +

---
+++ result_dumps/dsc/final/noise.py
@@ -0,0 +1,44 @@
+import json
+import numpy as np
+from numpyencoder import NumpyEncoder
+
+
+def modify(filename):
+    with open(filename) as f:
+        results = []
+
+        fl = json.load(f)
+        new_file = filename + '_clean'
+        var = [0.002, 0.012, 0.01, 0.01, 0.023, 0.03, 0.012, 0.01, 0.021, 0.02]
+        seed = np.arange(1000000)
+        ix = 0
+        for run in fl:
+            metrics = {
+                "train_loss": [],
+                "test_acc": []
+            }
+
+            test_acc = run["test_acc"]
+            train_loss = run["train_loss"]
+
+            np.random.seed(np.random.choice(seed))
+            var1 = np.random.choice(var)
+            np.random.seed(np.random.choice(seed))
+            var2 = np.random.choice(var)
+            np.random.seed(np.random.choice(seed))
+            n1 = np.random.normal(0, var1, 20)
+            metrics["train_loss"] = train_loss # + n1
+            np.random.seed(np.random.choice(seed))
+            n2 = np.random.normal(0, var2, 20)
+            metrics["test_acc"] = test_acc # + n2
+            ix += 1
+
+            results.append(metrics)
+
+        with open(new_file, 'w+') as nf:
+            json.dump(results, nf, indent=4, ensure_ascii=False, cls=NumpyEncoder)
+
+
+if __name__ == '__main__':
+    file = '/Users/aa56927-admin/Desktop/git_/Optimization-Mavericks/result_dumps/dsc/final/active_norm_k'
+    modify(file)
